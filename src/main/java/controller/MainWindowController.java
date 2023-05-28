d85a0857da9d415db73935f97da3503f102cc18b
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2023-05-28 22:35:10 +0330

    - Design of the application's main window  is completed.
    - Main window's controller created but not developed.

---
+++ src/main/java/controller/MainWindowController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+    @FXML
+    private Button editCustomerBtn;
+
+    @FXML
+    private Button flightsListBtn;
+
+    @FXML
+    private TextField fullNameField;
+
+    @FXML
+    private Button loginSignupBtn;
+
+    @FXML
+    private Button orderHistoryBtn;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField usernameField;
+
+}
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        VBox root = FXMLLoader.load(this.getClass().getResource("../MainWindow.fxml"));
+        primaryStage.setTitle("Travel Agency");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
